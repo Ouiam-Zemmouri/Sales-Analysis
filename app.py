cb6a3670c933e7de633095edb1d5b272e0b794c8
Ouiam-Zemmouri <ouiam.zemmouri@coficab.com>
2026-05-19 13:53:07 +0100

    Update page title in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 st.set_page_config(
-    page_title="LME Sales Analysis 2026",
+    page_title="Sales Analysis 2026",
     page_icon="⚡",
     layout="wide",
     initial_sidebar_state="expanded",
